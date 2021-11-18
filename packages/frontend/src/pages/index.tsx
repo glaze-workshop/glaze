540552bf1e1f86e8c991d68e5d6c86e180a2bfa4
吴新宇 <kunduin@outlook.com>
2021-11-18 20:04:06 +0800

    Upgrade react router.

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,23 +1,21 @@
 import React, { FC, Suspense } from 'react'
-import { Redirect, Route, Router, Switch } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom'
 import history from '../utils/customHistory'
 
 const LazyEditor = React.lazy(() => import('./editor/index'))
 
 const RootPage: FC = () => {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path='/'>
-          <Redirect to='/editor' />
-        </Route>
-        <Route path='/editor'>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/editor' />}/>
+        <Route path='/editor' element={
           <Suspense fallback={'loading'}>
             <LazyEditor />
           </Suspense>
-        </Route>
-      </Switch>
-    </Router>
+        }/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
