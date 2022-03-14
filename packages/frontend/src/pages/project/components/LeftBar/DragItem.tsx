b909c65db454bd1fb083dcc7f00a68ac8650e1c4
cclintris <181250083@smail.nju.edu.cn>
2022-03-14 14:13:41 +0800

    add: 完成左側邊欄基本組建設計(具體創建元素接下來搞)

--- packages/frontend/src/pages/project/components/LeftBar/DragItem.tsx
+++ packages/frontend/src/pages/project/components/LeftBar/DragItem.tsx
@@ -1,20 +1,47 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { FieldNode } from '../../../../schema/config'
+import { Box } from '@chakra-ui/react'
 import cl from 'classnames'
+import { Rnd } from 'react-rnd'
 
 export interface DragItemProps {
   fieldItem: FieldNode
 }
 
 const DragItem: FC<DragItemProps> = ({ fieldItem }: DragItemProps) => {
+  const [isDragging, setIsDragging] = useState(false)
+
   return (
-    <div
-      className={cl(
-        'p-2 border border-gray-200 text-center text-gray-600 shadow-sm rounded-sm bg-gray-50 cursor-move hover:bg-gray-100 hover:text-gray-900 hover:border-indigo-500'
-      )}
+    <Rnd
+      default={{
+        x: 0,
+        y: 0,
+        width: '100%',
+        height: '40px'
+      }}
+      onDrag={(e) => e && setIsDragging(true)}
+      onDragStop={(e) => e && setIsDragging(false)}
     >
-      {fieldItem.type}
-    </div>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        h="40px"
+        bg="gray.50"
+        border="1px"
+        borderColor="gray.100"
+        shadow="sm"
+        fontSize="medium"
+        cursor="move"
+        rounded="md"
+        className={cl('hover:border-indigo-500', {
+          'opacity-50': isDragging
+        })}
+      >
+        {fieldItem.type}
+      </Box>
+    </Rnd>
   )
 }
 
--- packages/frontend/src/pages/project/components/LeftBar/DragPanel.tsx
+++ packages/frontend/src/pages/project/components/LeftBar/DragPanel.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { FieldNode } from '../../../../schema/config'
 import DragItem from './DragItem'
+import { Box } from '@chakra-ui/react'
 
 export interface DragPanelProps {
   fields: FieldNode[]
@@ -8,10 +9,10 @@ export interface DragPanelProps {
 
 export const DragPanel: FC<DragPanelProps> = ({ fields }: DragPanelProps) => {
   return (
-    <>
+    <Box position="relative" w="90%" h="100%" margin="0 auto">
       {fields.map((item) => (
         <DragItem key={item.type} fieldItem={item} />
       ))}
-    </>
+    </Box>
   )
 }
