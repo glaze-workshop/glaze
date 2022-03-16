9299eb7ee9972189a34528e6f4cf9bac56a03d08
superfree <superfreeeee@gmail.com>
2022-03-16 15:05:43 +0800

    Add useBehaviorSubjectValue hook

--- packages/frontend/src/hooks/utils.hook.ts
+++ packages/frontend/src/hooks/utils.hook.ts
@@ -1,6 +1,25 @@
-import { useReducer } from 'react'
+import { useEffect, useReducer, useState } from 'react'
+import { BehaviorSubject } from 'rxjs'
 
 export function useForceRerender () {
   const [, forceRender] = useReducer((s: number) => s + 1, 0)
   return forceRender
+}
+
+/**
+ * Turn BehaviorSubject into React state
+ */
+export const useBehaviorSubjectValue = <T>(subject: BehaviorSubject<T>): T => {
+  const [value, setValue] = useState(subject.value)
+
+  useEffect(() => {
+    const subscriber = subject.subscribe((value) => {
+      setValue(value)
+    })
+    return () => {
+      subscriber.unsubscribe()
+    }
+  }, [subject])
+
+  return value
 }
