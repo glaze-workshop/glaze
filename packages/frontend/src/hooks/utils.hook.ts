bcfe3c114653faa213c03a8eb5e98a008459afcc
superfree <superfreeeee@gmail.com>
2022-03-16 15:13:23 +0800

    Update BehaviorSubject hooks

--- packages/frontend/src/hooks/utils.hook.ts
+++ packages/frontend/src/hooks/utils.hook.ts
@@ -1,13 +1,54 @@
-import { useEffect, useReducer, useState } from 'react'
+import { useCallback, useEffect, useReducer, useState } from 'react'
 import { BehaviorSubject } from 'rxjs'
 
-export function useForceRerender () {
+export function useForceRerender() {
   const [, forceRender] = useReducer((s: number) => s + 1, 0)
   return forceRender
 }
 
 /**
- * Turn BehaviorSubject into React state
+ * useState for BehaviorSubject
+ * ex:
+ *   ```ts
+ *   const [value, setValue] = useBehaviorSubjectState(valueSubject)
+ *   ```
+ */
+export const useBehaviorSubjectState = <T>(
+  subject: BehaviorSubject<T>
+): [T, (nextValue: T) => void] => {
+  const [value, setValue] = useState(subject.value)
+
+  /**
+   * Subscribe Updating
+   */
+  useEffect(() => {
+    const subscriber = subject.subscribe((value) => {
+      setValue(value)
+    })
+    return () => {
+      subscriber.unsubscribe()
+    }
+  }, [subject])
+
+  /**
+   * Setter
+   */
+  const setSubject = useCallback(
+    (nextValue: T) => {
+      subject.next(nextValue)
+    },
+    [subject]
+  )
+
+  return [value, setSubject]
+}
+
+/**
+ * state for BehaviorSubject
+ * ex:
+ *   ```ts
+ *   const value = useBehaviorSubjectValue(valueSubject)
+ *   ```
  */
 export const useBehaviorSubjectValue = <T>(subject: BehaviorSubject<T>): T => {
   const [value, setValue] = useState(subject.value)
@@ -22,4 +63,24 @@ export const useBehaviorSubjectValue = <T>(subject: BehaviorSubject<T>): T => {
   }, [subject])
 
   return value
+}
+
+/**
+ * setState for BehaviorSubject
+ * ex:
+ *   ```ts
+ *   const setValue = useSetBehaviorSubjectState(valueSubject)
+ *   ```
+ */
+export const useSetBehaviorSubjectState = <T>(
+  subject: BehaviorSubject<T>
+): ((nextValue: T) => void) => {
+  const setSubject = useCallback(
+    (nextValue: T) => {
+      subject.next(nextValue)
+    },
+    [subject]
+  )
+
+  return setSubject
 }
