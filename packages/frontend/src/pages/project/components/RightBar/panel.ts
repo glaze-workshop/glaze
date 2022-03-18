c2a06f376b53447ee3844cda3c7e7dbf36ad48fa
cclintris <181250083@smail.nju.edu.cn>
2022-03-18 17:20:31 +0800

    commit to push

---
+++ packages/frontend/src/pages/project/components/RightBar/panel.ts
@@ -0,0 +1,8 @@
+import { PositionType } from '../../../../schema/layout'
+
+export const positionOption: Array<PositionType> = [
+  PositionType.TOP,
+  PositionType.BOTTOM,
+  PositionType.LEFT,
+  PositionType.RIGHT
+]
